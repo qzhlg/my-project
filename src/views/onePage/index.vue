c059eff4df3c25b669be3908b3be983ba81eaa09
unknown <ligege_v@didiglobal.com>
2020-12-04 16:36:27 +0800

    修改

--- src/views/onePage/index.vue
+++ src/views/onePage/index.vue
@@ -1,9 +1,27 @@
 <template>
     <div>
-        11111
+        <H1>{{msg}}</H1>
+        <button @click="addMethod">点击</button>
+        <p>this number will {{count}}</p>
     </div>
 </template>
 <script>
+import Vue from "vue";
+export default Vue.extend({
+    name:'Data',
+    data(){
+        return {
+            msg:'hello word!',
+            count:0
+        }
+    },
+    methods:{
+        addMethod(){
+            this.count += 1
+        }
+    }
+ 
+})
 </script>
 
 <style scoped>
